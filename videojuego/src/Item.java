a7c4de04325beaa9f8a999bb8effc60999a89cde
Kata <123377593+cterron074@users.noreply.github.com>
2026-02-19 20:15:25 +0100

    Update19/02 Item.java

--- videojuego/src/Item.java
+++ videojuego/src/Item.java
@@ -5,18 +5,50 @@ public class Item {
      private int attackPoints;
      private int defenPoints;
      private int healthPoints;
-    
 
-
-
-
-
-
-
-
-
-
-
-
+     public String getNombre() {
+          return nombre;
      }
-    
+     public void setNombre(String nombre) {
+          this.nombre = nombre;
+     }
+     public int getPrize() {
+          return prize;
+     }
+     public void setPrize(int prize) {
+          this.prize = prize;
+     }
+     public Tipo getTipoItem() {
+          return tipoItem;
+     }
+     public void setTipoItem(Tipo tipoItem) {
+          this.tipoItem = tipoItem;
+     }
+     public int getAttackPoints() {
+          return attackPoints;
+     }
+     public void setAttackPoints(int attackPoints) {
+          this.attackPoints = attackPoints;
+     }
+     public int getDefenPoints() {
+          return defenPoints;
+     }
+     public void setDefenPoints(int defenPoints) {
+          this.defenPoints = defenPoints;
+     }
+     public int getHealthPoints() {
+          return healthPoints;
+     }
+     public void setHealthPoints(int healthPoints) {
+          this.healthPoints = healthPoints;
+     }
+     public Item(String nombre, int prize, Tipo tipoItem, int attackPoints, int defenPoints, int healthPoints) {
+          this.nombre = nombre;
+          this.prize = prize;
+          this.tipoItem = tipoItem;
+          this.attackPoints = attackPoints;
+          this.defenPoints = defenPoints;
+          this.healthPoints = healthPoints;
+     }
+     
+     }
