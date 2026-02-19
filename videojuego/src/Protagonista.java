a062553567e0a35aee4de2291ba4eb3ff27efd09
Kata <123377593+cterron074@users.noreply.github.com>
2026-02-19 20:13:42 +0100

    Update19/02 Protagonista.java

--- videojuego/src/Protagonista.java
+++ videojuego/src/Protagonista.java
@@ -6,6 +6,36 @@ public class Protagonista {
     private Item armadura;
     private int oro;
     private ArrayList<Item> bolsa;
+    public Personaje getProtagonista() {
+        return protagonista;
+    }
+    public void setProtagonista(Personaje protagonista) {
+        this.protagonista = protagonista;
+    }
+    public Item getArmadura() {
+        return armadura;
+    }
+    public void setArmadura(Item armadura) {
+        this.armadura = armadura;
+    }
+    public int getOro() {
+        return oro;
+    }
+    public void setOro(int oro) {
+        this.oro = oro;
+    }
+    public ArrayList<Item> getBolsa() {
+        return bolsa;
+    }
+    public void setBolsa(ArrayList<Item> bolsa) {
+        this.bolsa = bolsa;
+    }
+    public Protagonista(Personaje protagonista, Item armadura, int oro, ArrayList<Item> bolsa) {
+        this.protagonista = protagonista;
+        this.armadura = armadura;
+        this.oro = oro;
+        this.bolsa = bolsa;
+    }
     
-
+    
 }
