9426f73b338af24cfe06cd4ef7af6a1a4fe22cfe
Kata <123377593+cterron074@users.noreply.github.com>
2026-02-19 20:17:17 +0100

    Update19/02 Enemigo.java

--- videojuego/src/Enemigo.java
+++ videojuego/src/Enemigo.java
@@ -2,12 +2,30 @@ public class Enemigo {
     private int vecesDerrotado;
     private int oroDerrotado;
     private Personaje personaje;
+
+    public Enemigo(int vecesDerrotado, int oroDerrotado, Personaje personaje) {
+        this.vecesDerrotado = vecesDerrotado;
+        this.oroDerrotado = oroDerrotado;
+        this.personaje = personaje;
+    }
+
+    public int getVecesDerrotado() {
+        return vecesDerrotado;
+    }
+    public void setVecesDerrotado(int vecesDerrotado) {
+        this.vecesDerrotado = vecesDerrotado;
+    }
+    public int getOroDerrotado() {
+        return oroDerrotado;
+    }
+    public void setOroDerrotado(int oroDerrotado) {
+        this.oroDerrotado = oroDerrotado;
+    }
+    public Personaje getPersonaje() {
+        return personaje;
+    }
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
+    }
     
-
-    /*
-    -
-Veces derrotado. (Int)
--
-Oro que proporciona al ser derrotado. (Int) */
-
 }
